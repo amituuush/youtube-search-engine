c87ba9a2f16348aed50720feeb15dfd3f3bf791e
Amit Ranan <ranan.amit@gmail.com>
2015-09-12 21:17:17 -0700

    2nd commit

--- apps/app.js
+++ apps/app.js
@@ -1,1 +1,25 @@
-alert("hey")
+$(document).ready(function() {
+
+	$('#search-term').submit(function(event) {
+		$('#search-results').html('');
+		var query = $('#query').val();
+		getRequest(query);
+		event.preventDefault();
+	});
+
+	function getRequest(searchTerm) {
+		$.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + searchTerm + '&key=AIzaSyDqKHNsExc8g5ttufklcS3o9oqcyZE5Oso', function(data) {
+			showResults(data.items);
+			console.log(data);
+		})
+	};
+
+	function showResults(results) {
+		for (var i = 0; i < results.length; i++) {
+		var myData = results[i].snippet.thumbnails.medium.url;
+		$('#search-results').append('<img src="' + myData + '">');
+		}
+
+	};
+	
+});
